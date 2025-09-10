d1b806a88db6163bbb66eef66f595403bdaa4c5f
Duong <nguyenthaiduong17082003@gmail.com>
2025-09-10 08:16:50 +0700

    product 4

--- src/composables/useProduct.js
+++ src/composables/useProduct.js
@@ -1,34 +1,17 @@
 import { useProductStore } from '@/stores/productStore'
-import { computed, ref, onMounted } from 'vue'
-import { CACHE_TTL } from '@/config/cache.config'
+import { computed, onMounted } from 'vue'
+
 
 export function useProduct(autoLoad = true) {
   const productStore = useProductStore()
-  const lastFetched = ref(null)
 
   const products = computed(() => productStore.products)
   const loading = computed(() => productStore.loading)
   const error = computed(() => productStore.error)
 
-  const fetchProducts = async (force = false) => {
-    const now = Date.now()
-
-    if (
-      !force &&
-      products.value.length > 0 &&
-      lastFetched.value &&
-      now - lastFetched.value < CACHE_TTL.PRODUCT_LIST
-    ) {
-      console.log('[STORE] Using cached products')
-      return products.value
-    }
-    console.log(1);
-
+  const fetchProducts = async () => {
     await productStore.fetchProducts()
-    lastFetched.value = now
-    return products.value
   }
-
   if (autoLoad) {
     onMounted(() => {
       fetchProducts()
