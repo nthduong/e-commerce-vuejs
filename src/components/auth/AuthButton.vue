97a146051fc0485f97fb80b478bf7b14f654b1c5
Duong <nguyenthaiduong17082003@gmail.com>
2025-11-27 11:13:55 +0700

    cart 3

--- src/components/auth/AuthButton.vue
+++ src/components/auth/AuthButton.vue
@@ -5,6 +5,10 @@ const prop = defineProps({
   logout: Function,
 })
 
+const logOut = () => {
+  prop.logout()
+}
+
 console.log(prop.currentUser)
 </script>
 
@@ -21,8 +25,23 @@ console.log(prop.currentUser)
   </template>
   <template v-else>
     <div class="header-actions">
-      <div class="header-actions__wrap-icon">
+      <div class="header-actions__wrap-icon user">
         <img class="header-actions__icon" src="@/assets/icons/user.svg" alt="" />
+        <div class="user__list">
+          <div v-if="currentUser.name" class="user__text">Hello!</div>
+          <div class="user__item">
+            <img class="user__icon" src="@/assets/icons/profile.svg" alt="icon profile" />
+            Profile
+          </div>
+          <div class="user__item">
+            <img class="user__icon" src="@/assets/icons/setting.svg" alt="icon setting" />
+            Setting
+          </div>
+          <div class="user__item" @click="logOut">
+            <img class="user__icon" src="@/assets/icons/logout.svg" alt="icon logout" />
+            Logout
+          </div>
+        </div>
       </div>
       <div class="header-actions__wrap-icon cart">
         <img class="header-actions__icon" src="@/assets/icons/cart.svg" alt="" />
@@ -46,12 +65,12 @@ console.log(prop.currentUser)
 
   &__wrap-icon {
     width: 28px;
-    cursor: pointer;
   }
 
   &__icon {
     width: 100%;
     height: 100%;
+    cursor: pointer;
   }
 }
 
@@ -71,6 +90,58 @@ console.log(prop.currentUser)
     top: -8px;
     right: -6px;
     background: var(--color-button-bg);
+  }
+}
+
+.user {
+  position: relative;
+
+  &:hover &__list {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  &__list {
+    min-width: 150px;
+    position: absolute;
+    background: #fff;
+    padding: 5px;
+    top: 35px;
+    right: 0;
+    box-shadow: var(--box-shadow);
+    border: 1px solid var(--auth-border);
+    border-radius: 10px;
+    opacity: 0;
+    visibility: hidden;
+    transition:
+      opacity 0.25s ease,
+      visibility 0.25s ease;
+  }
+
+  &__item {
+    font-size: 14px;
+    padding: 8px 12px;
+    border-radius: 8px;
+    white-space: nowrap;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    user-select: none;
+    cursor: pointer;
+    &:hover {
+      background: #f5f5f5;
+    }
+  }
+
+  &__text {
+    padding: 8px 12px;
+    white-space: nowrap;
+    font-weight: 500;
+    user-select: none;
+  }
+
+  &__icon {
+    width: 20px;
   }
 }
 </style>
